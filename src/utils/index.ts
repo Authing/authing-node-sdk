d886b49f4058675097b9e9c391de77c97a47b6a4
zkb <zhengkaibin@authing.cn>
2022-04-25 19:58:21 +0800

    fix: buffer not support in browser

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,26 +11,29 @@ export const pickBy = (
   return ret;
 };
 
+// support in The buffer module from node.js, for the browser.
+var Buffer = require('buffer/').Buffer
+
 export function parseJwt(token: string) {
-  const base64Url = token.split(".")[1];
-  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   const jsonPayload = decodeURIComponent(
-    Buffer.from(base64, "base64")
+    Buffer.from(base64, 'base64')
       .toString()
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      .split('')
+      .map(function (c: string) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       })
-      .join("")
+      .join('')
   );
 
   return JSON.parse(jsonPayload);
 }
 
 export function generateRandomString(length: number = 30) {
-  let result = "";
+  let result = '';
   const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -39,7 +42,7 @@ export function generateRandomString(length: number = 30) {
 }
 
 export function generateRandomEmail(length: number = 30) {
-  return `${generateRandomString(length)}` + "@example.com";
+  return `${generateRandomString(length)}` + '@example.com';
 }
 
 /**
@@ -48,40 +51,40 @@ export function generateRandomEmail(length: number = 30) {
  */
 export const generateRandomPhone = () => {
   const headerNums = [
-    "139",
-    "138",
-    "137",
-    "136",
-    "135",
-    "134",
-    "159",
-    "158",
-    "157",
-    "150",
-    "151",
-    "152",
-    "188",
-    "187",
-    "182",
-    "183",
-    "184",
-    "178",
-    "130",
-    "131",
-    "132",
-    "156",
-    "155",
-    "186",
-    "185",
-    "176",
-    "133",
-    "153",
-    "189",
-    "180",
-    "181",
-    "177",
+    '139',
+    '138',
+    '137',
+    '136',
+    '135',
+    '134',
+    '159',
+    '158',
+    '157',
+    '150',
+    '151',
+    '152',
+    '188',
+    '187',
+    '182',
+    '183',
+    '184',
+    '178',
+    '130',
+    '131',
+    '132',
+    '156',
+    '155',
+    '186',
+    '185',
+    '176',
+    '133',
+    '153',
+    '189',
+    '180',
+    '181',
+    '177',
   ];
   const headerNum = headerNums[Math.floor(Math.random() * 10)];
-  const bodyNum = Math.random().toString().replace("0.", "").slice(0, 8);
+  const bodyNum = Math.random().toString().replace('0.', '').slice(0, 8);
   return headerNum + bodyNum;
 };
