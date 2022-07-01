f2c72a427bd31a050659602f20f8875522581ae9
Willin Wang <willin@willin.org>
2022-07-01 10:17:34 +0800

    chore: bump to v0.0.13

--- src/models/DepartmentDto.ts
+++ src/models/DepartmentDto.ts
@@ -6,9 +6,17 @@ import type { I18nDto } from './I18nDto';
 
 export type DepartmentDto = {
     /**
+     * 组织 Code（organizationCode）
+     */
+    organizationCode: string;
+    /**
      * 部门系统 ID（为 Authing 系统自动生成，不可修改）
      */
     departmentId: string;
+    /**
+     * 部门创建时间
+     */
+    createdAt: string;
     /**
      * 自定义部门 ID，用于存储自定义的 ID
      */
@@ -17,6 +25,10 @@ export type DepartmentDto = {
      * 部门名称
      */
     name: string;
+    /**
+     * 部门负责人 ID
+     */
+    leaderUserIds?: Array<string>;
     /**
      * 部门描述
      */
@@ -30,6 +42,14 @@ export type DepartmentDto = {
      */
     code?: string;
     /**
+     * 部门人数（仅包含直属成员）
+     */
+    membersCount: number;
+    /**
+     * 是否包含子部门
+     */
+    hasChildren: boolean;
+    /**
      * 是否是虚拟部门
      */
     isVirtualNode?: boolean;
@@ -37,4 +57,8 @@ export type DepartmentDto = {
      * 多语言设置
      */
     i18n?: I18nDto;
+    /**
+     * 部门的扩展字段数据
+     */
+    customData?: any;
 };
