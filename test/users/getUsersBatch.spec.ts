d221f647183f0cff39a6d5fc9418fc573d4fdaa9
Willin Wang <willin@willin.org>
2022-07-14 10:10:12 +0800

    test: getUsersBatch temp

---
+++ test/users/getUsersBatch.spec.ts
@@ -0,0 +1,41 @@
+import { managementClient } from "../client";
+
+describe("getUsersBatch", () => {
+  let userIds: string[] = [];
+
+  beforeAll(async () => {
+    const { data } = await managementClient.listUsers({
+      withCustomData: true,
+      withIdentities: true,
+      withDepartmentIds: true,
+    });
+    userIds = data.list.map((x) => x.userId);
+  });
+
+  describe("Success", () => {
+    it("default", async () => {
+      const { statusCode, data } = await managementClient.getUsersBatch({
+        userIds,
+        withCustomData: true,
+        withIdentities: true,
+        withDepartmentIds: true,
+      });
+      expect(statusCode).toEqual(200);
+      expect(data.length).toBeGreaterThan(0);
+      const user = data?.[0];
+      expect(user.customData).toBeDefined();
+      expect(user.departmentIds).toBeDefined();
+      expect(user.identities).toBeDefined();
+    });
+  });
+
+  describe("Fail", () => {
+    it("userIds is invalid", async () => {
+      const { statusCode, message } = await managementClient.getUsersBatch({
+        userIds: [],
+      });
+      expect(statusCode).toEqual(400);
+      expect(message).toEqual("参数 userIds 格式错误");
+    });
+  });
+});
