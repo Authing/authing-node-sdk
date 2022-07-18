448c1eb51f1699d9273dd97f6ff0e702e09bdd0e
Willin Wang <willin@willin.org>
2022-07-18 10:01:36 +0800

    chore: bump to v0.0.19

--- src/models/CreateNamespaceDto.ts
+++ src/models/CreateNamespaceDto.ts
@@ -3,16 +3,16 @@
 /* eslint-disable */
 
 export type CreateNamespaceDto = {
-  /**
-   * 权限分组唯一标志符
-   */
-  code?: string;
-  /**
-   * 权限分组名称
-   */
-  name?: string;
-  /**
-   * 权限分组描述信息
-   */
-  description?: string;
+    /**
+     * 权限分组唯一标志符
+     */
+    code: string;
+    /**
+     * 权限分组名称
+     */
+    name?: string;
+    /**
+     * 权限分组描述信息
+     */
+    description?: string;
 };
