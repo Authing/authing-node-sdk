4c43bd5bb58ff5eaedd3bf374998be5d240c75e1
Willin Wang <willin@willin.org>
2023-02-05 11:46:20 +0800

    test: createTenant

---
+++ test/tenant/createTenant.spec.ts
@@ -0,0 +1,42 @@
+import { generateRandomString } from "../../src/utils";
+import { managementClient } from "../client";
+
+describe("createTenant", () => {
+  const name = generateRandomString(10);
+
+  beforeAll(async () => {});
+
+  describe("Success", () => {
+    it("with full basic tenant", async () => {
+      const {
+        statusCode,
+        data: tenant,
+        message,
+      } = await managementClient.createTenant({
+        name,
+      });
+
+      expect(statusCode).toEqual(200);
+      expect(tenant.name).toEqual(name);
+    });
+  });
+
+  // 析构;
+  afterAll(async () => {
+    //删除用户
+    // const { statusCode, data, message } =
+    //   await managementClient.deleteDepartment({
+    //   });
+  });
+
+  describe("Fail", () => {
+    it("tenant name must be not empty", async () => {
+      const {
+        statusCode,
+        data: tenant,
+        message,
+      } = await managementClient.createTenant({});
+      expect(statusCode).toEqual(400);
+    });
+  });
+});
