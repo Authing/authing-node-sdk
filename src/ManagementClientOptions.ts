663275790d7e4da4273f89190fdc11ee3341f5ab
zhaojingjing <zhaojingjing@authing.com>
2023-02-28 20:01:06 +0800

    feat(all): socket 默认地址修改

--- src/ManagementClientOptions.ts
+++ src/ManagementClientOptions.ts
@@ -54,6 +54,6 @@ export const DEFAULT_OPTIONS: ManagementClientOptions = {
   accessKeyId: "",
   accessKeySecret: "",
   rejectUnauthorized: true,
-  socketUri: "wss://events.authing.com/",
+  socketUri: "wss://events.authing.com",
   retryTimes: 5
 };
