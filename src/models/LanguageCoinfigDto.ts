835d2a45b5feabe37a4f91f9b7542bb2db5a3192
师英英 <shiyingying@authing.cn>
2023-04-12 15:46:29 +0800

    feat:tenant-2.0.6

--- src/models/LanguageCoinfigDto.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-import type { LanguageCoinfigGlobalDto } from './LanguageCoinfigGlobalDto';
-
-export type LanguageCoinfigDto = {
-    global: LanguageCoinfigGlobalDto;
-};
